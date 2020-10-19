a84d4874a8898ac8be027c32ace71640c6ab0e00
aditi <aditi@DESKTOP-343N0FS>
2020-10-19 21:58:16 +0700

    Upload Project Tugas

---
+++ src/if1/pkg10119016/latihan4/kambing/IF110119016Latihan4Kambing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119016.latihan4.kambing;
+
+/**
+ *
+ * @author aditi
+ * NAMA              : Aditya Ilham Subagja
+ * KELAS             : IF1
+ * NIM               : 10119016
+ * Deskripsi Program : Program ini berisi program untuk menambah nilai
+ */
+public class IF110119016Latihan4Kambing {
+    
+    public void tambahKambing(){
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah:" + jumlahKambing);
+       
+    }
+
+    public static void main(String[] args) {
+    IF110119016Latihan4Kambing kambingJantan = new IF110119016Latihan4Kambing();
+    kambingJantan.tambahKambing();
+    }
+    
+}
